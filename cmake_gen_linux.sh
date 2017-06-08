9d900f8da6773d993f03cf652b74a5482d00a3fd
stefan.graenitz <stefan.graenitz@gmail.com>
2017-06-08 18:29:17 +0200

    Initial commit: LLVM Kaleidoscope example (Chapter 9), CMakeLists, Fibonacci test input

---
+++ cmake_gen_linux.sh
@@ -0,0 +1,9 @@
+export CC=clang-3.8
+export CXX=clang++-3.8
+
+if [ ! -d "build" ]; then
+    mkdir build
+fi
+
+cd build
+cmake -DCMAKE_BUILD_TYPE=Debug -DLLVM_DIR=/media/LLVM/build-llvm40-clang-lldb/lib/cmake/llvm ../src
